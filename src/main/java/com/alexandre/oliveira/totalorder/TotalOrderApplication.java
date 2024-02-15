0c24c6e2c01be98ba59ad48a84e9f5a3161dc012
AlexandreCDEO <acdo13031991@gmail.com>
2024-02-15 19:47:46 -0300

    first commit

---
+++ src/main/java/com/alexandre/oliveira/totalorder/TotalOrderApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.oliveira.totalorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandre/oliveira/totalorder/TotalOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.oliveira.totalorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
